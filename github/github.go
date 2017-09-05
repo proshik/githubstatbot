692def485b6107d07fa8333039428b24ecb6d6e2
proshik <proxoraleksandrovich@gmail.com>
2017-09-06 00:30:37 +0300

    Get languages statistic from github

--- github/github.go
+++ github/github.go
@@ -3,12 +3,18 @@ package github
 import (
 	"net/http"
 	"encoding/json"
-	_"fmt"
 	"fmt"
+	"log"
+	"sort"
 )
 
-type Repos struct {
-	Repo []*Repo
+type GHAuth struct {
+	Token string
+}
+
+type LangStatistic struct {
+	Language   string
+	Percentage float32
 }
 
 type Repo struct {
@@ -16,37 +22,91 @@ type Repo struct {
 	Name string `json:"name"`
 }
 
-type Language struct {
-}
+var client = http.Client{}
 
-func allRepos(profile string) (map[string]int, error) {
-	resp, err := http.Get("https://api.github.com/users/" + profile + "/repos")
+func (auth *GHAuth) AllRepos(profile string) ([]*LangStatistic, error) {
+	url := fmt.Sprintf("https://api.github.com/users/%s/repos", profile)
+
+	resp, err := get(auth, url)
 	if err != nil {
 		return nil, err
 	}
 
 	defer resp.Body.Close()
 
-	var data []*Repo
-	json.NewDecoder(resp.Body).Decode(&data)
+	var repos []*Repo
+	json.NewDecoder(resp.Body).Decode(&repos)
 
-	result := make(map[string]int, 0)
-
-	for _, d := range data {
-		resp1, err := http.Get("https://api.github.com/repos/" + profile + "/" + d.Name + " /languages")
+	result := make(map[string]int)
+	for _, repo := range repos {
+		lang, err := getLanguages(profile, repo.Name, auth)
 		if err != nil {
-			return nil, err
+			log.Printf("Error on request language statistic for repository=%s with err=%v", repo.Name, err)
 		}
-		defer resp1.Body.Close()
 
-		var langs map[string]int
-		json.NewDecoder(resp1.Body).Decode(&langs)
-
-
-
-		result = append(result, langs[])
+		for k, v := range lang {
+			result[k] = result[k] + v
+		}
 	}
 
-	return make(map[string]int, 0), nil
+	return calcPercentages(result), nil
+}
 
+func getLanguages(profile string, repoName string, auth *GHAuth) (map[string]int, error) {
+
+	url := fmt.Sprintf("http://api.github.com/repos/%s/%s/getLanguages", profile, repoName)
+
+	resp, err := get(auth, url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var langMap map[string]int
+	json.NewDecoder(resp.Body).Decode(&langMap)
+
+	return langMap, nil
+}
+
+func get(auth *GHAuth, url string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", "token "+auth.Token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func calcPercentages(languages map[string]int) []*LangStatistic {
+
+	result := make([]*LangStatistic, 0)
+
+	var totalSum float32
+
+	for _, v := range languages {
+		totalSum += float32(v)
+	}
+
+	for key, value := range languages {
+		percent := float32(value) * (float32(100) / totalSum)
+		result = append(result, &LangStatistic{key, round(percent, 0.1)})
+	}
+
+	sort.Slice(result, func(i, j int) bool { return result[i].Percentage > result[j].Percentage })
+
+	return result
+}
+
+func round(x, unit float32) float32 {
+	if x > 0 {
+		return float32(int32(x/unit+0.5)) * unit
+	}
+	return float32(int32(x/unit-0.5)) * unit
 }
--- github/github_test.go
+++ github/github_test.go
@@ -1,10 +1,17 @@
 package github
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+	"sort"
+)
 
-func TestAllRepos(t *testing.T){
+func TestAllRepos(t *testing.T) {
+	token := "token"
 
-	result, err := allRepos("proshik")
+	auth := &GHAuth{token}
+
+	result, err := auth.AllRepos("proshik")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -12,4 +19,31 @@ func TestAllRepos(t *testing.T){
 	if len(result) <= 0 {
 		t.Fatal("Lenght of map is empty")
 	}
+
+	fmt.Printf("Result: %s\n", result)
+}
+
+func TestCalcPercentages(t *testing.T) {
+	languages := map[string]int{
+		"Java": 199992,
+		"Go":   16172,
+		"HTML": 13579}
+
+	expected := []*LangStatistic{
+		{"Java", 87.1},
+		{"Go", 7},
+		{"HTML", 5.9},
+	}
+	sort.Slice(expected, func(i, j int) bool {
+		return expected[i].Percentage > expected[j].Percentage
+	})
+
+	result := calcPercentages(languages)
+
+	for i := 0; i < len(result); i++ {
+		if *result[i] != *expected[i] {
+			t.Errorf("CalcPercentages = expected=%v, actual=%v", expected[i], result[i])
+		}
+	}
+	//fmt.Sprintf("%.1f", result[0])
 }
