d9a689e2523bda98adbb5da650eccd745664331b
proshik <proxoraleksandrovich@gmail.com>
2018-01-08 23:53:30 +0300

    Fix compile errors.

--- storage/db.go
+++ storage/db.go
@@ -14,7 +14,7 @@ type Store struct {
 }
 
 func New(path string) *Store {
-	db, err := bolt.Open(path, 0666, &bolt.Options{Timeout: 1 * time.Second})
+	db, err := bolt.Open(path, 0777, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		panic(err)
 	}
